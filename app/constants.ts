73d15417d837c63d9be9a015028105dfd459f84a
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-08-28 03:13:50 +0700

    fix: vercel url

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
 import { Connection, clusterApiUrl } from '@solana/web3.js'
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
 export const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
 export const TILE_SIZE = 64
